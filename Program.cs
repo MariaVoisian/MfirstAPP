4ea0f2ff678060c8da5b734cfe090d7d399f074f
Maria Voisian <m.voisian@yahoo.com>
2021-10-04 10:07:48 +0300

    my class

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace MyfirstAPP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("my class!");
         }
     }
 }
